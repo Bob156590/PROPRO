eb6b8d99e37e16885ddfc9bd4dfa6c3c0e753cea
Bob156590 <111962487+Bob156590@users.noreply.github.com>
2022-09-09 11:01:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Uppgift_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorit book?");
+            string book = Console.ReadLine();
+            Console.WriteLine("so your favorite book is " + book + ". That's a good book");
+        }
+    }
+}
