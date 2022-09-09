be84f14eba77f78f97e1801eed0df79e69315edf
Bob156590 <111962487+Bob156590@users.noreply.github.com>
2022-09-09 11:17:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Uppgift_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("how many meters did Elin jump?");
+            string jump= Console.ReadLine();
+            Console.WriteLine("And how meter did Alma jump?");
+            string jump2= Console.ReadLine();
+            int sub= int.Parse(jump);
+            int sub2= int.Parse(jump2);
+            int summan = sub - sub2;
+            Console.WriteLine("So Elin jumped " + summan + "m futher then Alma.");
+        }
+    }
+}
