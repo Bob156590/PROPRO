9f051b035737ca5083f4512b07fdb99baaa696d6
Bob156590 <111962487+Bob156590@users.noreply.github.com>
2022-09-14 11:38:21 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,18 +1,19 @@
 ﻿using System;
-namespace Uppgift_2._2
+namespace Uppgift_2._1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("how many meters did Elin jump?");
-            string jump= Console.ReadLine();
-            Console.WriteLine("And how meter did Alma jump?");
-            string jump2= Console.ReadLine();
-            int sub= int.Parse(jump);
-            int sub2= int.Parse(jump2);
-            int summan = sub - sub2;
-            Console.WriteLine("So Elin jumped " + summan + "m futher then Alma.");
+            Console.WriteLine("welcome to the car rental company calculater.");
+            Console.WriteLine("so how many day will you rent our car?");
+            string days=Console.ReadLine();
+            Console.WriteLine("and for how many km do will you be driving?");
+            string km=Console.ReadLine();
+            int days2= int.Parse(days);
+            int km2= int.Parse(km);
+            int money=500 * days2 + km2 + 300;
+            Console.WriteLine("So if we add everything together that comes to" + money + "kr");
         }
     }
 }
