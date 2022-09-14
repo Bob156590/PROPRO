97ed6c2f8a03015c09cbb8f11a7bf5e136d561ae
Bob156590 <111962487+Bob156590@users.noreply.github.com>
2022-09-14 12:02:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Uppgift_2_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pls write the wages of the three employes one by on.");
+            int money= int.Parse(Console.ReadLine());
+            money= money+ int.Parse(Console.ReadLine());
+            money= money+ int.Parse(Console.ReadLine());
+            Console.WriteLine("so if add all the wages together the add up to " + money + "kr");
+        }
+    }
+}
